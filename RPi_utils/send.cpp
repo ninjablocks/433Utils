776b25330204543c3b16701b8904405f3000f68a
rhoudin <fablabs41@gmail.com>
2015-10-18 23:22:58 +0200

    Update send.cpp

--- RPi_utils/send.cpp
+++ RPi_utils/send.cpp
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]) {
     if (wiringPiSetup () == -1) return 1;
 	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
 	RCSwitch mySwitch = RCSwitch();
+	if (argv[4] != NULL) mySwitch.setPulseLength(atoi(argv[4]));
 	mySwitch.enableTransmit(PIN);
     
     switch(command) {
